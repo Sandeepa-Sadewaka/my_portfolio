09a3166e23fb3bd10084ae51e0569e29cf9791d4
Sandeepa-Sadewaka <sadewakasandeepa39@gmail.com>
2025-03-07 13:33:22 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -23,30 +23,35 @@ export const Contact = () => {
         transition={{ duration: 1, delay: 0.4 }}
       >
         <li className={styles.link}>
-          <motion.img
+            <a href="mailto:sadewakasandeepa39@gmail.com">
+            <motion.img
             src={"assets/contact/emailIcon.png"}
             alt="Email icon"
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.2 }}
-          />
+          /></a>
           <a href="mailto:sadewakasandeepa39@gmail.com">email</a>
         </li>
         <li className={styles.link}>
+          <a href="https://www.linkedin.com/in/sandeepa-nadeeshan-32384b311/">
           <motion.img
             src={"assets/contact/linkedinIcon.png"}
             alt="LinkedIn icon"
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.2 }}
           />
+          </a>
           <a href="https://www.linkedin.com/in/sandeepa-nadeeshan-32384b311/">linkedin</a>
         </li>
         <li className={styles.link}>
+          <a href="https://github.com/Sandeepa-Sadewaka">
           <motion.img
             src={"assets/contact/githubIcon.png"}
             alt="Github icon"
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.2 }}
           />
+          </a>
           <a href="https://github.com/Sandeepa-Sadewaka">github</a>
         </li>
       </motion.ul>
